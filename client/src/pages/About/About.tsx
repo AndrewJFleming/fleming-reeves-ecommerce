efaea894ebd1db498311122155337bcde7cf074c
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-05-02 08:59:52 -0700

    Replace vanilla jsx elements with Typography components in About component.

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,23 +1,26 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
-import { Container } from "@mui/material";
+
+import { Container, Typography } from "@mui/material";
 
 interface Props {}
 
 export const About: FC<Props> = () => {
   return (
     <Container>
-      <h1>About</h1>
-      <p>
+      <Typography variant="h4" my="1rem">
+        About
+      </Typography>
+      <Typography mb="2rem">
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
         ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
         dis parturient montes, nascetur ridiculus mus. Donec quam felis,
         ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
         quis enim.
-      </p>
-      <p>
+      </Typography>
+      <Typography>
         Back to <Link to="/">products page</Link>.
-      </p>
+      </Typography>
     </Container>
   );
 };
