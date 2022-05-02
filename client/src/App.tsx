663e8acf88f89343e1c08f4a5a0f9ebf90d5e40d
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-05-02 08:51:45 -0700

    -Create custom MUI theme with createTheme.
    -Replace default palette with custom color palette.
    -import GoogleFonts and replace default theme fonts.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import axios, { AxiosResponse } from "axios";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 //Page Components
 import Home from "./pages/Home/Home";
@@ -10,10 +11,70 @@ import { Auth } from "./pages/Auth/Auth";
 import SingleProduct from "./pages/SingleProduct/SingleProduct";
 
 //Components
-import { TopNav } from "./components/TopNav";
+import { TopNav } from "./components/TopNav/TopNav";
 
 import { ProductData } from "./interfaces";
 import "./App.css";
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#20002d",
+      light: "#706378",
+      dark: "#16001c",
+    },
+    secondary: {
+      main: "#be6e85",
+      light: "#d0abb9",
+      dark: "#7d3d4a",
+    },
+    error: {
+      main: "#c44343",
+      light: "#d09398",
+      dark: "#86271e",
+    },
+    warning: {
+      main: "#ed943f",
+      light: "#e7b594",
+      dark: "#a46616",
+    },
+    success: {
+      main: "#f8ebb3",
+      light: "#f6edd5",
+      dark: "#a19b69",
+    },
+  },
+  typography: {
+    fontFamily: "Bree Serif",
+    h1: {
+      fontFamily: "Bree Serif",
+    },
+    h2: {
+      fontFamily: "Bree Serif",
+    },
+    h3: {
+      fontFamily: "Bree Serif",
+    },
+    h4: {
+      fontFamily: "Bree Serif",
+    },
+    h5: {
+      fontFamily: "Bree Serif",
+    },
+    h6: {
+      fontFamily: "Bree Serif",
+    },
+    body1: {
+      fontFamily: "Open Sans",
+    },
+    body2: {
+      fontFamily: "Open Sans",
+    },
+    button: {
+      fontFamily: "Open Sans",
+    },
+  },
+});
 
 function App() {
   const [user, setUser] = useState(false);
@@ -30,20 +91,22 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <TopNav user={user} />
-      <Routes>
-        <Route element={<Home productData={products} />} path="/" />
-        <Route element={<About />} path="/about"></Route>
-        <Route element={<Contact />} path="/contact"></Route>
-        <Route element={<Auth title="Login" />} path="/login"></Route>
-        <Route element={<Auth title="Register" />} path="/register"></Route>
-        <Route
-          element={<SingleProduct productData={products} />}
-          path="/products/:productId"
-        />
-      </Routes>
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <div className="App">
+        <TopNav user={user} />
+        <Routes>
+          <Route element={<Home productData={products} />} path="/" />
+          <Route element={<About />} path="/about"></Route>
+          <Route element={<Contact />} path="/contact"></Route>
+          <Route element={<Auth title="Login" />} path="/login"></Route>
+          <Route element={<Auth title="Register" />} path="/register"></Route>
+          <Route
+            element={<SingleProduct productData={products} />}
+            path="/products/:productId"
+          />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
