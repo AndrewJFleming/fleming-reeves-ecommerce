d0ee969293dd33538f906b78f9f040a671533dc6
TechnoGecko <cr5697@gmail.com>
2022-05-02 15:40:42 -0700

    fixing merge conflicts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,29 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import SingleProduct from './pages/SingleProduct/SingleProduct';
-import { TopNav } from './components/TopNav';
 import axios, { AxiosResponse } from 'axios';
-import './App.css';
-import { ProductData } from './interfaces';
-
-function App() {
-  const [user, setUser] = useState(true);
-=======
-import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import axios, { AxiosResponse } from "axios";
 
 //Page Components
-import Home from "./pages/Home/Home";
-import { About } from "./pages/About/About";
-import { Contact } from "./pages/Contact/Contact";
-import { Auth } from "./pages/Auth/Auth";
-import SingleProduct from "./pages/SingleProduct/SingleProduct";
+import Home from './pages/Home/Home';
+import { About } from './pages/About/About';
+import { Contact } from './pages/Contact/Contact';
+import { Auth } from './pages/Auth/Auth';
+import SingleProduct from './pages/SingleProduct/SingleProduct';
 
 //Components
-import { TopNav } from "./components/TopNav";
+import { TopNav } from './components/TopNav/TopNav';
 
-import { ProductData } from "./interfaces";
-import "./App.css";
+import { ProductData } from './interfaces';
+import './App.css';
 
 function App() {
   const [user, setUser] = useState(false);
->>>>>>> master
   const [products, setProducts] = useState<ProductData[]>([]);
 
-  console.log("Products: ", products);
+  console.log('Products: ', products);
 
   useEffect(() => {
     axios
-<<<<<<< HEAD
       .get<ProductData[]>('http://localhost:5000/products')
-=======
-      .get<ProductData[]>("http://localhost:5000/products")
->>>>>>> master
       .then((response: AxiosResponse) => {
         setProducts(response.data);
       });
@@ -52,13 +34,10 @@ function App() {
       <TopNav user={user} />
       <Routes>
         <Route element={<Home productData={products} />} path="/" />
-<<<<<<< HEAD
-=======
-        <Route element={<About />} path="/about"></Route>
-        <Route element={<Contact />} path="/contact"></Route>
-        <Route element={<Auth title="Login" />} path="/login"></Route>
-        <Route element={<Auth title="Register" />} path="/register"></Route>
->>>>>>> master
+        <Route element={<About />} path="/about" />
+        <Route element={<Contact />} path="/contact" />
+        <Route element={<Auth title="Login" />} path="/login" />
+        <Route element={<Auth title="Register" />} path="/register" />
         <Route
           element={<SingleProduct productData={products} />}
           path="/products/:productId"
---
+++ client/src/components/TopNav/CartNavItemInner/CartNavItemInner.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+interface Props {
+  cartCount: number;
+}
+const CartNavItemInner: FC<Props> = ({ cartCount }) => {
+  return (
+    <div style={{ position: "relative" }}>
+      <ShoppingCartIcon
+        fontSize="small"
+        sx={{ position: "absolute", top: "1px", right: "17px" }}
+      />
+      <span style={{ marginLeft: "17px", fontWeight: "300" }}>
+        &nbsp;({cartCount})
+      </span>
+    </div>
+  );
+};
+
+export default CartNavItemInner;
