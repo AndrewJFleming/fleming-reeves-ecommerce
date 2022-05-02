0d0290f54a4fd85345534c075f53d1fb8dca0598
TechnoGecko <cr5697@gmail.com>
2022-05-02 15:05:30 -0700

    added search bar

--- client/src/components/Products/Product/Product.tsx
+++ client/src/components/Products/Product/Product.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import "./Product.css";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import './Product.css';
+import { Link } from 'react-router-dom';
 
 interface ProductProps {
   imageId: string;
@@ -9,17 +9,26 @@ interface ProductProps {
   desc: string;
 }
 
-const Product = ({ imageId, imageUrl, title, desc }: ProductProps) => {
+const Product = ({
+  imageId,
+  imageUrl,
+  title,
+  desc
+}: ProductProps) => {
   return (
-    <Link className="product-card_link" to={"/products/" + imageId}>
+    <Link className="product-card_link" to={'/products/' + imageId}>
       <div className="product-card">
         <img
           className="product-card_image"
           src={imageUrl}
           alt="Thumbnail image of the artwork being sold"
         />
-        <h2 className="product-card_title">{title}</h2>
-        <h3 className="product-card_desc">{desc}</h3>
+        <h2 className="product-card_title">
+          {title}
+        </h2>
+        <h3 className="product-card_desc">
+          {desc.length <= 99 ? desc : `${desc.substring(0, 100)}...`}
+        </h3>
       </div>
     </Link>
   );
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,63 +1,98 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import Box, { BoxProps } from '@mui/material/Box';
 import Products from '../../components/Products/Products';
 import Pagination from '@mui/material/Pagination/';
 import Stack from '@mui/material/Stack';
 import { ProductData } from '../../interfaces';
 import './Home.css';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 type HomeProps = {
   productData: ProductData[];
-}
+};
 
-const Home = ({productData}: HomeProps) => { 
+const Home = ({ productData }: HomeProps) => {
   const [productsArray, setProductsArray] = useState(productData);
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage, setProductsPerPage] = useState(5);
+  const [searchString, setSearchString] = useState('');
 
   useEffect(() => {
-    setProductsArray(productData)
-  }, productData)
+    setProductsArray(productData);
+  }, productData);
 
-  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
     setCurrentPage(value);
-  }
+  };
 
-  const handlePageNumberChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handlePageNumberChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     const value = event.target.value;
     setProductsPerPage(parseInt(value));
-  }
+  };
 
   const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage
-  const numberOfPages = Math.ceil(productsArray.length / productsPerPage)
-  const currentProducts = productsArray.slice(indexOfFirstProduct, indexOfLastProduct);
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const numberOfPages = Math.ceil(
+    productsArray.length / productsPerPage
+  );
+  const currentProducts = productsArray.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  const filteredProducts = productsArray.filter(
+    product => product.title == searchString
+  );
 
   return (
     <div className="home-page_container">
-    <Box  
-    sx={{
-      display: 'grid',
-      gap: 1,
-      gridTemplateColumns: '(3, 1fr)',
-    }}>
-      <Products productsArray={currentProducts}></Products>
-      <div className="pagination-container">
-      
-      </div>
-    </Box>
-    <Stack className="pagination" spacing={2} sx={{ justifyContent: 'center'}}>
-        <Pagination count={numberOfPages}  page={currentPage} onChange={handlePageChange} shape="rounded"/>
-        <label className="pagination-dropdown_label" htmlFor="products-per-page">Items per page:</label>
-        <select onChange={handlePageNumberChange} className="pagination-dropdown_menu" name="products-per-page">
+      <SearchBar
+        placeholder={'Search by title'}
+        data={productsArray}
+      />
+      <Box
+        sx={{
+          display: 'grid',
+          gap: 1,
+          gridTemplateColumns: '(3, 1fr)'
+        }}
+      >
+        <Products productsArray={currentProducts} />
+        <div className="pagination-container" />
+      </Box>
+      <Stack
+        className="pagination"
+        spacing={2}
+        sx={{ justifyContent: 'center' }}
+      >
+        <Pagination
+          count={numberOfPages}
+          page={currentPage}
+          onChange={handlePageChange}
+          shape="rounded"
+        />
+        <label
+          className="pagination-dropdown_label"
+          htmlFor="products-per-page"
+        >
+          Items per page:
+        </label>
+        <select
+          onChange={handlePageNumberChange}
+          className="pagination-dropdown_menu"
+          name="products-per-page"
+        >
           <option value={5}>5</option>
           <option value={10}>10</option>
           <option value={15}>15</option>
         </select>
-
       </Stack>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
