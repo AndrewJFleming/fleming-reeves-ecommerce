17d1e15eb6e40b9887aa3be240359a3d542d0e55
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-05-05 12:21:34 -0700

    Refactor Auth page forms to dispatch formData
    -Add SwitchAuthPage components and clear formData state on link click.

---
+++ client/src/pages/Auth/SwitchAuthPage/SwitchAuthPage.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
+
+import { Typography } from "@mui/material";
+
+interface Props {
+  prompt: string;
+  title: string;
+  altPath: string;
+  handleClearForm: any;
+}
+
+export const SwitchAuthPage: FC<Props> = ({
+  title,
+  prompt,
+  altPath,
+  handleClearForm,
+}) => {
+  return (
+    <Typography>
+      {prompt}
+      <Link to={altPath} onClick={handleClearForm}>
+        {title}
+      </Link>
+    </Typography>
+  );
+};
