7c44f9549ed7d82e1a151b119be1071317dd6e34
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-05-17 15:13:52 -0700

    Use MUI card components for Favorites cards.

--- client/src/pages/Favorites/Favorites.tsx
+++ client/src/pages/Favorites/Favorites.tsx
@@ -1,17 +1,22 @@
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { updateFavorites } from "../../redux/features/users";
+import { makeStyles } from "@mui/styles";
+import { ProductData } from "../../interfaces";
+import BackButton from "../../components/BackButton/BackButton";
+
 import {
+  Container,
+  CardContent,
+  CardMedia,
+  Card,
   Box,
   IconButton,
   List,
-  ListItem,
-  ListItemText,
   Typography,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { makeStyles } from "@mui/styles";
-import { useDispatch } from "react-redux";
-import { ProductData } from "../../interfaces";
-import { useNavigate } from "react-router-dom";
-import { updateFavorites } from "../../redux/features/users";
 
 type Props = {
   favoritesIds: string[];
@@ -36,35 +41,54 @@ const Favorites = ({ favorites, userId, favoritesIds }: Props) => {
 
   let allFavorites = favorites?.map((favorite) => {
     return (
-      <ListItem
-        alignItems="flex-start"
-        sx={{
-          width: "85%",
-          border: "1px solid black",
-          borderRadius: 7,
-          margin: "15px 0px",
-        }}
-      >
-        <img src={favorite.squareThumbUrl} alt="a preview of the product" />
-        <ListItemText
-          primary={favorite.title}
-          primaryTypographyProps={{ fontSize: "24px", fontWeight: 600 }}
-          sx={{
-            alignSelf: "center",
-            marginLeft: "3vw",
-            fontSize: "24px",
-          }}
-        />
-        <p>{favorite._id}</p>
-        <IconButton onClick={() => handleRemove(favorite._id)}>
-          <DeleteIcon />
-        </IconButton>
-      </ListItem>
+      <Card sx={{ display: "flex", marginBottom: 2 }}>
+        <Link to={"/products/" + favorite._id}>
+          <CardMedia
+            component="img"
+            sx={{ width: 151 }}
+            image={favorite.squareThumbUrl}
+            alt="Favorite product image thumbnail"
+          />
+        </Link>
+        <Box sx={{ display: "flex", width: 300, flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto", pb: 0 }}>
+            <Link to={"/products/" + favorite._id}>
+              <Typography component="div" variant="h6">
+                {favorite.title}
+              </Typography>
+            </Link>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {favorite._id}
+            </Typography>
+          </CardContent>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              pl: 1,
+              pb: 1,
+            }}
+          >
+            <IconButton
+              aria-label="Remove from favorites"
+              onClick={() => handleRemove(favorite._id)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </Box>
+        </Box>
+      </Card>
     );
   });
 
   return (
-    <Box>
+    <Container>
+      <BackButton />
       <Typography variant="h2" textAlign="center">
         Favorites
       </Typography>
@@ -73,11 +97,12 @@ const Favorites = ({ favorites, userId, favoritesIds }: Props) => {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          margin: "35px 0px 0px",
         }}
       >
         {allFavorites}
       </List>
-    </Box>
+    </Container>
   );
 };
 
