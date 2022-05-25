8a981f00e3c939e87373bc3d1f4dcf003b863e82
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-05-24 18:23:32 -0700

    Add CartState, CartItemsState, UserState and UserReducerState to interfaces.ts

--- client/src/interfaces.ts
+++ client/src/interfaces.ts
@@ -1,26 +1,53 @@
 export interface ProductData {
-        _id: string;
-		title: string;
-		desc: string;
-		price: number;
-		categories?: Array<string>;
-		isFeatured: boolean;
-		squareThumbUrl: string;
-		largeUrl: string;
-		fullsizeUrl: string;
-		variants?: Variants[],
-		createdAt: string;
-		updatedAt: string;
-		__v: number;
+  _id: string;
+  title: string;
+  desc: string;
+  price: number;
+  squareThumbUrl: string;
+  largeUrl: string;
+  fullsizeUrl: string;
+  categories?: Array<string>;
+  isFeatured?: boolean;
+  variants?: Variants[];
+  createdAt?: string;
+  updatedAt?: string;
+  __v?: number;
 }
 
 export interface Variants {
-    id: string;
-    name: string;
-    squareThumbUrl: string;
-    largeUrl: string;
-    fullsizeUrl: string;
-
-            
+  id: string;
+  name: string;
+  squareThumbUrl: string;
+  largeUrl: string;
+  fullsizeUrl: string;
 }
 
+export interface CartItemState {
+  pId: string;
+  title: string;
+  thumbnail: string;
+  price: number;
+  quantity: number;
+}
+
+export interface CartState {
+  cartItems: [CartItemState];
+}
+
+export interface UserState {
+  _id: string;
+  username: string;
+  email: string;
+  isAdmin: boolean;
+  favorites: string[];
+  createdAt?: string;
+  updatedAt?: string;
+  __v?: number;
+}
+
+export interface UserReducerState {
+  authData: {
+    user: UserState;
+  };
+  error: boolean | { name: string; message: string; createdAt: string };
+}
