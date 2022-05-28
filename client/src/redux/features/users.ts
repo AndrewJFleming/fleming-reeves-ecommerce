fb24e8b2d8bb167ceebe982fd28f54f5868d3165
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-05-28 11:13:33 -0700

    Specify argumentObj object properties types
    -Specify argumentObj object properties types for Auth related functions.

--- client/src/redux/features/users.ts
+++ client/src/redux/features/users.ts
@@ -1,9 +1,20 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { NavigateFunction } from "react-router-dom";
 import * as api from "../api";
+import axios from "axios";
 
-export const login:any = createAsyncThunk(
+export const login: any = createAsyncThunk(
   "user/signInUser",
-  async (argumentObj:any, thunkAPI ) => {
+  async (
+    argumentObj: {
+      loginFormData: {
+        username: string;
+        password: string;
+      };
+      navigate: NavigateFunction;
+    },
+    thunkAPI
+  ) => {
     try {
       const { data } = await api.login(argumentObj.loginFormData);
       argumentObj.navigate("/");
@@ -14,9 +25,19 @@ export const login:any = createAsyncThunk(
   }
 );
 
-export const register:any = createAsyncThunk(
+export const register: any = createAsyncThunk(
   "user/signUpUser",
-  async (argumentObj:any, thunkAPI ) => {
+  async (
+    argumentObj: {
+      formData: {
+        username: string;
+        password: string;
+        email: string;
+      };
+      navigate: NavigateFunction;
+    },
+    thunkAPI
+  ) => {
     try {
       const { data } = await api.register(argumentObj.formData);
       argumentObj.navigate("/");
@@ -27,18 +48,21 @@ export const register:any = createAsyncThunk(
   }
 );
 
-export const updateFavorites:any = createAsyncThunk(
+export const updateFavorites: any = createAsyncThunk(
   "user/updateUserFavorites",
-  async (argumentObj:any, thunkAPI ) => {
+  async (argumentObj: any, thunkAPI) => {
     try {
-      const { data } = await api.updateFavorites(argumentObj.userId, argumentObj.updatedFavoritesIds);
+      const { data } = await api.updateFavorites(
+        argumentObj.userId,
+        argumentObj.updatedFavoritesIds
+      );
       return data;
     } catch (error: any) {
       setTimeout(() => {
         //navigate passed from App.tsx in argumentObj arg
         thunkAPI.dispatch(logoutUser());
         argumentObj.navigate("/login");
-      }, 5000)
+      }, 5000);
       return thunkAPI.rejectWithValue(error.response.data);
     }
   }
@@ -48,91 +72,98 @@ export const updateUser: any = createAsyncThunk(
   "user/updateUser",
   async (argumentObj: any, thunkAPI) => {
     try {
-      const { data } = await api.updateUser(argumentObj.userId, argumentObj.updateFormData)
+      const { data } = await api.updateUser(
+        argumentObj.userId,
+        argumentObj.updateFormData
+      );
       argumentObj.navigate("/login");
-    } catch(error: any) {
+    } catch (error: any) {
       console.log(error);
       return thunkAPI.rejectWithValue(error.response.data);
     }
   }
-)
+);
 
 export const deleteUser: any = createAsyncThunk(
   "user/deleteUser",
-  async (argumentObj: any, thunkAPI ) => {
+  async (argumentObj: any, thunkAPI) => {
     try {
-      const { data } = await api.deleteUser(argumentObj.userId, argumentObj.deleteFormData);
+      const { data } = await api.deleteUser(
+        argumentObj.userId,
+        argumentObj.deleteFormData
+      );
       console.log(data);
       argumentObj.navigate("/login");
       return data;
     } catch (error: any) {
-      console.log(error)
+      console.log(error);
       return thunkAPI.rejectWithValue(error.response.data);
     }
   }
-)
+);
 
 const userSlice = createSlice({
-    name: "user",
-    initialState: { authData: {}, error: false, loading: false, },
-    reducers: {
-      logoutUser: (state) => {
-        //Also clear user data from local storage
-        localStorage.removeItem("profile");
-        state.authData = {};
-        state.error = false;
-        state.loading = false;
-      },
+  name: "user",
+  initialState: { authData: {}, error: false, loading: false },
+  reducers: {
+    logoutUser: (state) => {
+      //Also clear user data from local storage
+      localStorage.removeItem("profile");
+      state.authData = {};
+      state.error = false;
+      state.loading = false;
     },
-    //Used when calling APIs
-    extraReducers: (builder) => {
-      //LOGIN
-      builder.addCase(login.pending, (state:any, action:any) => {
+  },
+  //Used when calling APIs
+  extraReducers: (builder) => {
+    //LOGIN
+    builder
+      .addCase(login.pending, (state: any, action: any) => {
         state.loading = true;
-        state.error = false
+        state.error = false;
       })
-      .addCase(login.fulfilled, (state:any, action:any) => {
+      .addCase(login.fulfilled, (state: any, action: any) => {
         //Also add user data and token to localStorage
-        localStorage.setItem("profile", JSON.stringify({...action.payload}));
+        localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
         state.loading = false;
         state.authData = action.payload;
-        state.error = false
+        state.error = false;
       })
-      .addCase(login.rejected, (state:any, action:any) => {
+      .addCase(login.rejected, (state: any, action: any) => {
         state.loading = false;
-        state.authData = {}
+        state.authData = {};
         state.error = action.payload;
       })
       //REGISTER
-      .addCase(register.pending, (state:any, action:any) => {
+      .addCase(register.pending, (state: any, action: any) => {
         state.loading = true;
-        state.error = false
+        state.error = false;
       })
-      .addCase(register.fulfilled, (state:any, action:any) => {
+      .addCase(register.fulfilled, (state: any, action: any) => {
         //Also add user data and token to localStorage
-        localStorage.setItem("profile", JSON.stringify({...action.payload}));
+        localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
         state.loading = false;
         state.authData = action.payload;
-        state.error = false
+        state.error = false;
       })
-      .addCase(register.rejected, (state:any, action:any) => {
+      .addCase(register.rejected, (state: any, action: any) => {
         state.loading = false;
-        state.authData = {}
+        state.authData = {};
         state.error = action.payload;
       })
       //UPDATE_FAVORITES
-      .addCase(updateFavorites.pending, (state:any, action:any) => {
+      .addCase(updateFavorites.pending, (state: any, action: any) => {
         state.loading = true;
-        state.error = false
+        state.error = false;
       })
-      .addCase(updateFavorites.fulfilled, (state:any, action:any) => {
+      .addCase(updateFavorites.fulfilled, (state: any, action: any) => {
         state.loading = false;
         state.authData.user = action.payload;
-        state.error = false
+        state.error = false;
       })
-      .addCase(updateFavorites.rejected, (state:any, action:any) => {
+      .addCase(updateFavorites.rejected, (state: any, action: any) => {
         state.loading = false;
-        state.authData = {}
+        state.authData = {};
         state.error = action.payload;
       })
       //UPDATE USER
@@ -162,9 +193,9 @@ const userSlice = createSlice({
       .addCase(deleteUser.rejected, (state: any, action: any) => {
         state.error = true;
         state.loading = false;
-      })
-    },
-})
+      });
+  },
+});
 
 export const { logoutUser } = userSlice.actions;
 
